935b6eebbab90069a1525c3d9a0b45cd49eccacb
lmcgrana <lmcgrana@iu.edu>
2018-07-19 09:37:14 -0400

    Updates banner

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,28 @@ const runSequence = require('run-sequence');
 const del = require('del');
 const package = require('./package.json');
 
-// Create the string for the verion number banner.
-const banner = '/*! ' + package.name + ' - @version v' + package.version + ' */' + '\n' + '\n';
+// Get the current year for copyright in the banner
+const currentYear = new Date().getFullYear();
+
+const banner = `/*!
+ * ${package.name} - @version ${package.version}
+ *
+ * Copyright (c) ${currentYear}, The Trustees of Indiana University
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+`;
 
 // Development server
 gulp.task('browser-sync', function () {
