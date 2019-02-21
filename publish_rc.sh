9dd2ed3ca97e19a56c3b272154257a877b659dae
Rachel <racmocon@gmail.com>
2019-02-21 16:23:52 -0500

    Change version number method

--- publish_rc.sh
+++ publish_rc.sh
@@ -19,22 +19,6 @@ echo "Publishing from $CIRCLE_BRANCH"
 [[ $CIRCLE_BRANCH =~ [0-9]+\.[0-9]+\.[0-9]+ ]]
 VERSION=$BASH_REMATCH
 
-# Update package.json with the latest version number
-echo "Updating package.json version to $VERSION..."
-npm version $VERSION —no-git-tag-version —no-commit-hooks
-if [ $? -eq 0 ]; then
-    echo "Package.json updated to version $VERSION. Commiting updated package.json..."
-    git config credential.helper 'cache --timeout=120'
-    git config user.email "iubot@iu.edu"
-    git config user.name "iubot"
-    git commit -m "Circle CI: update package.json version. [skip ci]"
-    echo "Pushing updated package.json to origin..."
-    # Push quietly to prevent showing the token in log
-    git push -q https://${GH_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git $CIRCLE_BRANCH
-else
-    echo "Package.json was already at version $VERSION."
-fi
-
 # Get count of commits to the release/hotfix branch.
 # This will be the "RC" number, e.g. RC.1
 echo "Counting number of commits on branch: $COMMITS..."
@@ -46,7 +30,25 @@ else
     COMMITS=0
 fi
 
+# Set the RC version as a combo of the branch version and number of commits.
+RC_VERSION="$VERSION-rc.$COMMITS"
+
+# Update package.json with the latest version number
+echo "Updating package.json version to $RC_VERSION..."
+npm version $RC_VERSION —no-git-tag-version —no-commit-hooks
+if [ $? -eq 0 ]; then
+    echo "Package.json updated to version $RC_VERSION. Commiting updated package.json..."
+    git config credential.helper 'cache --timeout=120'
+    git config user.email "iubot@iu.edu"
+    git config user.name "iubot"
+    git commit -m "Circle CI: update package.json version. [skip ci]"
+    echo "Pushing updated package.json to origin..."
+    # Push quietly to prevent showing the token in log
+    git push -q https://${GH_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git $CIRCLE_BRANCH
+else
+    echo "Package.json was already at version $RC_VERSION."
+fi
+
 # Form the tag with the number of commits to this branch and publish the package.
-TAG="rc.$COMMITS"
-echo "Publishing package to NPM with tag '$TAG' ..."
-npm publish --tag "$TAG"
+echo "Publishing package to NPM tag 'rc' with version '$RC_VERSION' ..."
+npm publish --tag rc
