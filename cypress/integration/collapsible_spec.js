fc0a3914c9b93bf4318bb07e442dcf668141c364
iubot <iubot@iu.edu>
2019-04-09 11:42:01 -0400

    Removed beforeEach functions in favor of calling cy.visit() when necessary

--- cypress/integration/collapsible_spec.js
+++ cypress/integration/collapsible_spec.js
@@ -3,11 +3,9 @@ const COLLAPSIBLE_CONTENT = '#default-collapsible';
 const DEV_SERVER = 'http://localhost:3000';
 
 describe('Rivet collapsible interactions', function() {
-  beforeEach(function () {
-    cy.visit(DEV_SERVER)
-  })
-
   it('Click the default Rivet collapsible toggle button', function() {
+    cy.visit(DEV_SERVER);
+
     cy.get(COLLAPSIBLE_BUTTON)
       .click()
       .should('have.attr', 'aria-expanded', 'true');
@@ -27,36 +25,30 @@ describe('Rivet collapsible interactions', function() {
 });
 
 describe('Collapsible API methods', function() {
-  beforeEach(function () {
-    cy.visit(DEV_SERVER)
-  })
-
-  it('Call the .open() method', function () {
+  it('Call the .open() method', function() {
+    cy.visit(DEV_SERVER);
     cy.window().then(win => {
       const toggleButton = win.document.querySelector(COLLAPSIBLE_BUTTON);
       win.Collapsible.open(toggleButton);
     });
 
-    cy.get(COLLAPSIBLE_BUTTON)
-      .should('have.attr', 'aria-expanded', 'true');
+    cy.get(COLLAPSIBLE_BUTTON).should('have.attr', 'aria-expanded', 'true');
 
     cy.get(COLLAPSIBLE_CONTENT)
       .should('have.attr', 'aria-hidden', 'false')
       .and('be.visible');
   });
 
-  it('Call the .close() method', function () {
+  it('Call the .close() method', function() {
     cy.window().then(win => {
       const toggleButton = win.document.querySelector(COLLAPSIBLE_BUTTON);
       win.Collapsible.close(toggleButton);
     });
 
-    cy.get(COLLAPSIBLE_BUTTON)
-      .should('have.attr', 'aria-expanded', 'false');
+    cy.get(COLLAPSIBLE_BUTTON).should('have.attr', 'aria-expanded', 'false');
 
     cy.get(COLLAPSIBLE_CONTENT)
       .should('have.attr', 'aria-hidden', 'true')
       .and('be.hidden');
   });
-})
-
+});
